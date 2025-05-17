e7b051ced00b1ce3e500d115cc3fbcc3a5cb8371
Shubham-Poddar9 <poddarshubham258@gmail.com>
2025-05-17 21:08:53 +0545

    acp2-ds-1

--- acp-2-ds-1/acp2.py
+++ acp-2-ds-1/acp2.py
@@ -1,13 +1,11 @@
 def myfunction(n):
-    for i in range(0, n+1): 
+    for i in range(0, n+1):
         print("First Loop")
 
-    j = 1 
-    while j <= n + 1:  
-        print("Second Loop ", j)
-        j = j * 2  
+    j = 1
+    while(j <= n+1):
+        print("Second Loop", j)
+        j = j * 2
 
-    for i in range(0, 100): 
-        print("Third Loop")
-
-    print("Total Time Complexity: O(n)")
+    for i in range(0, 100):
+        print("Third loop")
